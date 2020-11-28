c42d75eb51b7b4d27359ab02b342c02ac2d8f764
technobulka@gmail.com <298519329@Github>
2020-11-28 23:46:38 +0300

    home work completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	"time"
+)
+
 func main() {
-	// Place your code here
+	t, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf(
+		"current time: %s\nexact time: %s\n",
+		time.Now().Round(0),
+		t.Round(0),
+	)
 }
