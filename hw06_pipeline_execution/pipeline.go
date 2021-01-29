0bf258e38e7d03df6db907fb0961d9322d05afec
Stanislav Kondratiev <technobulka@gmail.com>
2021-01-29 20:18:08 +0300

    Home work #5

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,4 +1,7 @@
 package hw06_pipeline_execution //nolint:golint,stylecheck
+import (
+	"sync"
+)
 
 type (
 	In  = <-chan interface{}
@@ -9,6 +12,46 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	out := make(Bi)
+
+	go func() {
+		wg := sync.WaitGroup{}
+
+		var chains = make([]Bi, len(stages))
+		for i := 0; i < len(stages); i++ {
+			chains[i] = make(Bi)
+		}
+
+		for i, do := range stages {
+			var next *Bi
+			if i == len(stages)-1 {
+				next = &out
+			} else {
+				next = &chains[i+1]
+			}
+
+			wg.Add(1)
+			go func(i int, do Stage) {
+				defer wg.Done()
+				defer close(*next)
+
+				for result := range do(chains[i]) {
+					*next <- result
+				}
+			}(i, do)
+		}
+
+		for item := range in {
+			select {
+			case <-done:
+				return
+			case chains[0] <- item:
+			}
+		}
+		close(chains[0])
+
+		wg.Wait()
+	}()
+
+	return out
 }
