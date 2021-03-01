54495a17cd1b6a558dbdb5d8d291d6997a4d2fb6
Stanislav Kondratiev <technobulka@gmail.com>
2021-03-01 16:09:13 +0300

    Home work #7 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,52 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+const (
+	testFile = "test_file.sh"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("no command", func(t *testing.T) {
+		code := RunCmd([]string{}, Environment{})
+		require.Equal(t, ExitCodeError, code)
+	})
+
+	t.Run("command not found", func(t *testing.T) {
+		createTestFile("#!/usr/bin/env bash\nqwe")
+		t.Cleanup(removeTestFile)
+		args := []string{"bash", testFile}
+
+		code := RunCmd(args, Environment{})
+		require.Equal(t, 127, code)
+	})
+
+	t.Run("no args", func(t *testing.T) {
+		createTestFile("#!/usr/bin/env bash\ngrep")
+		t.Cleanup(removeTestFile)
+		args := []string{"bash", testFile}
+
+		code := RunCmd(args, Environment{})
+		require.Equal(t, 2, code)
+	})
+}
+
+func createTestFile(code string) {
+	err := ioutil.WriteFile(testFile, []byte(code), 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func removeTestFile() {
+	err := os.Remove(testFile)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Println("usage: go-envdir /path/to/env/dir command arg1 arg2")
+		os.Exit(ExitCodeError)
+	}
+
+	envDir, command := args[1], args[2:]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(ExitCodeError)
+	}
+
+	result := RunCmd(command, env)
+	os.Exit(result)
 }
