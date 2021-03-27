530f7bcb545864e610711fbd506090966900f74b
Stanislav Kondratiev <technobulka@gmail.com>
2021-03-27 22:53:16 +0300

    Home work #10 is completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,70 +1,48 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
 	"strings"
 )
 
-type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
-}
-
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
+	domains, err := countDomains(r, domain)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
 	}
-	return countDomains(u, domain)
+	return domains, nil
 }
 
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	var i int
-
-	jd := json.NewDecoder(r)
-	for {
-		var user User
-
-		err = jd.Decode(&user)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				err = nil
-			}
-
-			break
-		}
-
-		result[i] = user
-		i++
-	}
-
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
+func countDomains(r io.Reader, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 	domain = "." + domain
+	var userLine string
+	var etIdx, quoteIdx int
 
-	for _, user := range u {
-		if user.ID == 0 {
-			break
+	br := bufio.NewReader(r)
+	for {
+		line, _, err := br.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, err
 		}
 
-		if strings.HasSuffix(user.Email, domain) {
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
+		userLine = string(line)
+		etIdx = strings.Index(userLine, "@")
+		quoteIdx = strings.Index(userLine[etIdx:], `"`)
+		userDomain := strings.ToLower(userLine[etIdx+1 : etIdx+quoteIdx])
+
+		if strings.HasSuffix(userDomain, domain) {
+			result[userDomain]++
 		}
 	}
+
 	return result, nil
 }
